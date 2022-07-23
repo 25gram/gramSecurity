2d991bad3078f430a08a477606ed90bb67f6dc48
HRleev <lyns6677@gmail.com>
2022-07-23 16:35:48 +0900

    일반회원수정

--- src/main/java/com/its/gramsecurity/service/MemberService.java
+++ src/main/java/com/its/gramsecurity/service/MemberService.java
@@ -3,7 +3,9 @@ package com.its.gramsecurity.service;
 import com.its.gramsecurity.repository.MemberRepository;
 import com.its.gramsecurity.dto.MemberDTO;
 import com.its.gramsecurity.entity.MemberEntity;
+import org.apache.tomcat.util.buf.UEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +15,9 @@ import java.util.Optional;
 public class MemberService {
     @Autowired
     private MemberRepository memberRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder encoder;
     public void save(MemberDTO memberDTO) {
         MemberEntity memberEntity= MemberEntity.toSaveEntity(memberDTO);
         memberRepository.save(memberEntity);
@@ -28,13 +33,19 @@ public class MemberService {
     }
     @Transactional
     public void update(MemberEntity memberEntity) {
-        //수정시에 영속성 컨텍스트 User오브젝트를 영속화 시키고, 영속화된 User 오브젝트를 수정
-        //select를 해서 User오브젝트를 DB로부터 가져오는 이유는 영속화를 하기 위해서
-        //영속화된 오브젝트를 변경하면 자동으로 DB 에 update문을 날려줌
-
+        //수정시에 영속성 컨텍스트 User 오브젝트를 영속화 시키고, 영속화된 User 오브젝트를 수정
+        //select 를 해서 User 오브젝트를 DB 로부터 가져오는 이유는 영속화를 하기 위해서
+        //영속화된 오브젝트를 변경하면 자동으로 DB 에 update 문을 날려줌
+        System.out.println("MemberService.update");
+        System.out.println("memberEntity = " + memberEntity);
         MemberEntity persistence= memberRepository.findByMemberId(memberEntity.getMemberId()).orElseThrow(()->{
             return new IllegalArgumentException("회원찾기실패");
         });
         String rawPassword=memberEntity.getMemberPassword();
+        String encPassword=encoder.encode(rawPassword);
+        persistence.setMemberPassword(encPassword);
+        persistence.setMemberIntro(memberEntity.getMemberIntro());
+
+
     }
 }
