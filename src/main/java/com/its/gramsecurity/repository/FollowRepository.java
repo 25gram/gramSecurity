32a206c91c88b1625e4a923ea88d13d16fb562c9
HRleev <lyns6677@gmailcom>
2022-07-28 11:15:05 +0900

    dd

--- src/main/java/com/its/gramsecurity/repository/FollowRepository.java
+++ src/main/java/com/its/gramsecurity/repository/FollowRepository.java
@@ -3,13 +3,8 @@ package com.its.gramsecurity.repository;
 import com.its.gramsecurity.entity.FollowEntity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 
 @Repository
 public interface FollowRepository extends JpaRepository<FollowEntity,Long> {
