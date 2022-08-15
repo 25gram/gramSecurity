63bf479620f2c53768f74222c383837658c8773d
havana1016 <hasangsu82@gmail.com>
2022-08-15 11:44:11 +0900

    dm페이지 개발중3

--- src/main/java/com/its/gramsecurity/controller/MsgController.java
+++ src/main/java/com/its/gramsecurity/controller/MsgController.java
@@ -1,6 +1,8 @@
 package com.its.gramsecurity.controller;
 
+import com.its.gramsecurity.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +13,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequiredArgsConstructor
 @RequestMapping("/msg")
 public class MsgController {
-
+@Autowired
+    MemberService ms;
 
 
     @GetMapping("msgpage")
     String msgpage(@RequestParam("loginId")String loginId, Model model){
-
+        model.addAttribute("mem",ms.findByLoginId(loginId));
         return "/msg";
     }
 }
